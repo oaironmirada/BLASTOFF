dd1e46cbcfc901d5b6f6eb320bd8011da1b7c7cc
oaironmirada <92761591+oaironmirada@users.noreply.github.com>
2021-10-21 01:34:20 -0300

    Add files via upload

---
+++ questao-14.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include<string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL, "Portuguese");
+	
+	int i, valor = 0;
+	char pal[225], inv[225];
+
+	printf("Digite uma palavra: ");
+	gets(pal);
+
+	for(i = 0; pal[i]; i++){
+		pal[i] = tolower(pal[i]);
+	}
+
+	strcpy(inv, pal);
+
+	strrev(inv);
+
+	valor = strcmp(pal, inv);
+
+	if (valor == 0){
+		printf("A palavra %s é palíndroma\n", pal);
+	}else{
+		printf("A palavra %s não é palíndroma\n", pal);
+	}
+   
+	return 0;
+}
